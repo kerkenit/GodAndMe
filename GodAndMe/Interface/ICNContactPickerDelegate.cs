4c0864cca4dbd813f5a14df3945acfcae0cfd337
Kerk en IT <info@kerkenit.nl>
2019-03-21 13:08:19 +0100

    Removed old items

--- GodAndMe/Interface/ICNContactPickerDelegate.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace GodAndMe.Interface
-{
-    [Foundation.Protocol(Name = "CNContactPickerDelegate", WrapperType = typeof(ContactPickerDelegate))]
-    [ObjCRuntime.Introduced(ObjCRuntime.PlatformName.iOS, 9, 0, ObjCRuntime.PlatformArchitecture.All, null)]
-    public interface ICNContactPickerDelegate : IDisposable, ObjCRuntime.INativeObject
-}
