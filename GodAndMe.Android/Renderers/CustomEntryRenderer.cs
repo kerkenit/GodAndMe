2d10bcaba4e02a23c93daa4d69d1e1f806025e89
Kerk en IT <info@kerkenit.nl>
2019-03-21 13:15:09 +0100

    Create CustomEntryRenderer.cs
    To show the money field on Android

---
+++ GodAndMe.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Android.Content;
+using Android.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryRenderer), typeof(GodAndMe.Droid.Renderers.CustomEntryRenderer))]
+namespace GodAndMe.Droid.Renderers
+{
+    class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (this.Control != null)
+            {
+                this.Control.InputType = InputTypes.ClassNumber | InputTypes.NumberFlagSigned | InputTypes.NumberFlagDecimal;
+            }
+        }
+    }
+}
