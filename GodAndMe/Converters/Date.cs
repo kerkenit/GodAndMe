b68382c6f9bf5936842b100b848fd11634128f28
Kerk en IT <info@kerkenit.nl>
2019-03-29 20:56:50 +0100

    Added dateformat binding
    Today, Tomorrow, Yesterday etc.

---
+++ GodAndMe/Converters/Date.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+namespace GodAndMe.Converters
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return CommonFunctions.i18n("Today");
+
+            if (value is DateTime)
+            {
+                DateTime dt = (DateTime)value;
+                if (dt.Date == DateTime.Today)
+                {
+                    return CommonFunctions.i18n("Today");
+                }
+                else if (dt.Date == DateTime.Today.AddDays(-2))
+                {
+                    return CommonFunctions.i18n("YesterdayDayBefore");
+                }
+                else if (dt.Date == DateTime.Today.AddDays(-1))
+                {
+                    return CommonFunctions.i18n("Yesterday");
+                }
+                else if (dt.Date == DateTime.Today.AddDays(1))
+                {
+                    return CommonFunctions.i18n("Tomorrow");
+                }
+                else if (dt.Date == DateTime.Today.AddDays(2))
+                {
+                    return CommonFunctions.i18n("TomorrowDayAfter");
+                }
+                else
+                {
+                    return dt.Date.ToString("D");
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
