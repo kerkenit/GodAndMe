0ac8360e2a2d1d3922cface9c7d918e272834ab9
Kerk en IT <info@kerkenit.nl>
2023-06-15 13:23:09 +0200

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,6 +8,13 @@ export interface Message {
   read: boolean;
 }
 
+export interface Intention {
+  id: number;
+  forName: string;
+  prayer: string;
+  date?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -71,6 +78,57 @@ export class DataService {
     }
   ];
 
+  public intentions: Intention[] = [
+    {
+      forName: 'Matt Chorsey',
+      prayer: 'New event: Trip to Vegas',
+      date: '9:32 AM',
+      id: 0,
+    },
+    {
+      forName: 'Lauren Ruthford',
+      prayer: 'Long time no chat',
+      date: '6:12 AM',
+      id: 1,
+    },
+    {
+      forName: 'Jordan Firth',
+      prayer: 'Report Results',
+      date: '4:55 AM',
+      id: 2,
+    },
+    {
+      forName: 'Bill Thomas',
+      prayer: 'The situation',
+      date: 'Yesterday',
+      id: 3,
+    },
+    {
+      forName: 'Joanne Pollan',
+      prayer: 'Updated invitation: Swim lessons',
+      date: 'Yesterday',
+      id: 4,
+    },
+    {
+      forName: 'Andrea Cornerston',
+      prayer: 'Last minute ask',
+      date: 'Yesterday',
+      id: 5,
+    },
+    {
+      forName: 'Moe Chamont',
+      prayer: 'Family Calendar - Version 1',
+      date: 'Last Week',
+      id: 6,
+    },
+    {
+      forName: 'Kelly Richardson',
+      prayer: 'Placeholder Headhots',
+      date: 'Last Week',
+      id: 7,
+    }
+  ];
+
   constructor() { }
 
   public getMessages(): Message[] {
@@ -79,5 +137,13 @@ export class DataService {
 
   public getMessageById(id: number): Message {
     return this.messages[id];
+  }
+
+  public getIntentions(): Intention[] {
+    return this.intentions;
+  }
+
+  public getIntentionById(id: number): Intention {
+    return this.intentions[id];
   }
 }
